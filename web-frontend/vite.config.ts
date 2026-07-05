034f12d162bf91711570cf1ac40049bd0ea9f197
ysksm <ka.sa.ma.123@gmail.com>
2026-07-06 00:47:52 +0900

    viteのproxy設定

--- web-frontend/vite.config.ts
+++ web-frontend/vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [react()],
 })
